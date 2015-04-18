4bbdca91f8b93522bdc068149cf1c7f4ad0272a2
Marco Biedermann <marco.biedermann@gmx.de>
2015-04-18 13:22:24 +0200

    add jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function (grunt) {
 
   'use strict';
 
+  require('jit-grunt')(grunt, {
+    cmq: 'grunt-combine-media-queries'
+  });
+
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
@@ -224,23 +228,6 @@ module.exports = function (grunt) {
     }
 
   });
-
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-combine-media-queries');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-processhtml');
-  grunt.loadNpmTasks('grunt-svgmin');
-  grunt.loadNpmTasks('grunt-svgstore');
 
   grunt.registerTask('default', ['sass', 'watch']);
   grunt.registerTask('test', [
