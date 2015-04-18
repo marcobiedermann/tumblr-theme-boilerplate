e17b65104468784955938dfd6fcfb073ed20d16e
Marco Biedermann <marco.biedermann@gmx.de>
2015-04-18 13:19:34 +0200

    add keyboard navigation

---
+++ source/js/script.js
@@ -0,0 +1,40 @@
+(function() {
+
+  'use strict';
+
+  // prev / next Post on Keypress
+
+  var prev = document.getElementById('prev');
+  var next = document.getElementById('next');
+
+  if (prev) {
+    var prevTarget = prev.href;
+  }
+
+  if (next) {
+    var nextTarget = next.href;
+  }
+
+  window.addEventListener('keydown', function(e) {
+
+    switch (e.keyCode) {
+
+      case  37: // left
+        if (prevTarget) {
+          window.location = prevTarget;
+        }
+        break;
+
+      case  39: // right
+        if (nextTarget) {
+          window.location = nextTarget;
+        }
+        break;
+
+      default: return;
+
+    }
+
+  });
+
+})();
